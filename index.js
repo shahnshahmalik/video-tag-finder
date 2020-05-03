031e92e6ee343669b8b9726f2662c65e6b1ddbd5
Rod McNew <rmcnew@relivinc.com>
2020-05-02 19:09:45 -0500

    add files

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-(()=>{
-    const div=document.createElement('div');
-    div.id='videoTagSourceList'
-    div.style.backgroundColor='green';
-    div.style.color='white';
-    div.innerHTML=[...document.getElementsByTagName('video')].length+' video tag(s) found:';
-    [...document.getElementsByTagName('video')].forEach((ele)=>{
+(() => {
+    const srcs = [...document.getElementsByTagName('video')].map((ele) => ele.src);
+    console.log('videoTagSourceList', srcs);
+    const div = document.createElement('div');
+    div.id = 'videoTagSourceList'
+    div.style.backgroundColor = 'green';
+    div.style.color = 'white';
+    div.innerHTML = srcs.length + ' video tag(s) found:';
+    srcs.forEach((src) => {
         var a = document.createElement('a');
-        a.href=ele.src
-        a.target='_blank'
-        a.innerHTML=ele.src
+        a.href = src
+        a.target = '_blank'
+        a.innerHTML = src
         div.append(document.createElement('br'));
         div.append(a)
     });
-    [...document.querySelectorAll('#videoTagSourceList')].forEach((ele)=>{
+    [...document.querySelectorAll('#videoTagSourceList')].forEach((ele) => {
         ele.remove()
-    })
+    });
     document.body.prepend(div);
 })();
