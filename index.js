588e9d9e7f06144f5870af34ca4ef93cfac89a55
Shahnshah <shahnshah.malik@protonmail.com>
2021-07-14 00:45:55 +0530

    UI changes

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 (() => {
     const div = document.createElement('div');
     // div.id = 'videoTagSourceList';
-    div.style.backgroundColor = '#00FF00';
-    div.style.color = 'black';
-    div.style.padding = '1em';
-    div.style.fontSize = '12pt'
+    div.style.backgroundColor = '#f3f4f9a2';
+    div.style.boxShadow = '0 0 3px rgb(108 124 139 / 50%)';
+    div.style.color = '#000';
+    div.style.padding = '1rem';
+    div.style.fontSize = '15px'
     div.style.zIndex = '2147483647';
-    div.style.position = 'absolute';
+    div.style.position = 'fixed';
     div.style.top = '0';
-    div.style.left = '0';
+    div.style.right = '0';
     div.style.overflow = 'hidden';
-    div.style.maxWidth = '100%'
+    div.style.minWidth = '300px';
+    div.style.maxWidth = '100%';
     const button = document.createElement('button');
-    button.innerHTML = 'X';
+    button.innerHTML = 'Close';
     button.style.float = 'right';
+    button.style.background = 'red';
+    button.style.border = 'none';
+    button.style.padding = '4px 8px';
+    button.style.borderRadius = '3px';
+    button.style.cursor = 'pointer';
+    button.style.color = '#fff';
     button.addEventListener('click', (event) => {
         clearInterval(interval);
         div.remove();
@@ -27,16 +35,13 @@
 
     const appendLink = (href) => {
         const appendA = document.createElement('a');
-        appendA.innerHTML = href;
+        appendA.innerHTML = '[CLICK FOR VIDEO]';
         appendA.href = href;
         appendA.target = '_blank';
         appendA.style.whiteSpace = 'nowrap';
         appendA.style.overflow = 'hidden';
         appendA.style.display = 'block';
         div.append(appendA);
-        // const video = document.createElement('video');
-        // video.src = href;
-        // div.append(video);
     }
 
     const render = () => {
